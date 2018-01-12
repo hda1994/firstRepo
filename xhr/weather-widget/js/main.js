f20a9e0c93c2ddcac2954f6e89a7edff0a507795
hda <hda1994@gmail.com>
2018-01-12 13:20:17 +0400

    home w 5

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,13 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
+request.addEventListener('load', onLoad);
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather');
 request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
+
+//if (request.status === 200) {
+//  const response = JSON.parse(request.responseText);
+//  setData(response);
+//}
+function onLoad(){
+	const response = JSON.parse(request.responseText);
+	setData(response);
 }
