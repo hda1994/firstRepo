b2622edc23a5f7783b8364d5bc6c18c188f08bcf
hda <hda1994@gmail.com>
2018-01-18 14:47:36 +0400

    home w 5  доработка ошибок

--- xhr/tabs/js/home.js
+++ xhr/tabs/js/home.js
@@ -1,53 +1,56 @@
 'use strict';
+let preloader = document.querySelector('#preloader');
+preloader.classList.remove('hidden');
+let xhr;
+document.addEventListener('DOMContentLoaded', () => {
+event.preventDefault();
+	onClick('email')
+});
+let content = document.querySelector('#content');
+let tabs = document.querySelectorAll('.tabs a');
+let email = tabs[0];
+let sms = tabs[1];
 
-		let xhre;
-		let xhrs;
-		document.addEventListener('DOMContentLoaded', onLoad);
-		let preloader = document.querySelector('#preloader');
-		let content = document.querySelector('#content');
-		let tabs = document.querySelectorAll('.tabs a');
-		let email = tabs[0];
-		let sms = tabs[1];
-		email.addEventListener('click', onclickEmail);
-		sms.addEventListener('click', onclickSms);
-		function onLoad(event){
-			onclickEmail(event);
-		}
-		function onProgress(event){
-			let pr = Math.round(event.loaded / event.total * 100);
-			preloader.innerHTML = 'Загрузка... ' + pr + '%';
-			preloader.classList.remove('hidden');
-		}
-		function onLoadEnd(){
-			preloader.classList.add('hidden');
-		}
-		function onLoadE(){
-			sms.classList.remove('active');
-			email.classList.add('active');
-			event.preventDefault();
-			content.innerHTML = xhre.responseText;
-		}
-		function onclickEmail(event){
-			event.preventDefault();
-			xhre = new XMLHttpRequest();
-			xhre.addEventListener('progress', onProgress);
-			xhre.addEventListener('load', onLoadE);
-			xhre.addEventListener('loadend', onLoadEnd);
-			xhre.open('GET', 'components/email-tab.html');
-			xhre.send();
-		}
-		function onLoadS(){
+email.addEventListener('click', () => {
+	event.preventDefault();
+	onClick('email')
+});
+sms.addEventListener('click', () => {
+	event.preventDefault();
+	onClick('sms')
+});
+
+function onLoadEnd(){
+	preloader.classList.add('hidden');
+}
+
+function onLoading(text){
+	switch(text){ 
+		case 'sms':
 			email.classList.remove('active');
 			sms.classList.add('active');
-			event.preventDefault();
-			content.innerHTML = xhrs.responseText;
-		}
-		function onclickSms(event){
-			event.preventDefault();
-			xhrs = new XMLHttpRequest();		
-			xhrs.addEventListener('progress', onProgress);
-			xhrs.addEventListener('load', onLoadS);
-			xhrs.addEventListener('loadend', onLoadEnd);
-			xhrs.open('GET', 'components/sms-tab.html');
-			xhrs.send();
-		}
+			break;
+		case 'email':
+			sms.classList.remove('active');
+			email.classList.add('active');
+			break;
+	}
+	content.innerHTML = xhr.responseText;
+}
+
+function onClick(text){
+	xhr = new XMLHttpRequest();
+	xhr.addEventListener('load',  () =>{
+		event.preventDefault();
+		onLoading(text);
+	});
+	xhr.addEventListener('loadend', onLoadEnd);
+	xhr.open('GET', 'components/' + text + '-tab.html');
+	xhr.send();
+}
+
+//		function onProgress(event){
+//			let pr = Math.round(event.loaded / event.total * 100);
+//			preloader.innerHTML = 'Загрузка... ' + pr + '%';
+//			preloader.classList.remove('hidden');
+//		}
